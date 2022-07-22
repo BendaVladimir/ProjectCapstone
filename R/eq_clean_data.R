c04cb05da7bb9b0e3bfb28cb5a4fa3814881c36f
BendaVladimir <benda.vladimir@gmail.com>
2022-07-22 05:23:10 -0400

    Adding global variables
    global variables were added to pass github actions

--- R/eq_clean_data.R
+++ R/eq_clean_data.R
@@ -1,15 +1,17 @@
+globalVariables(c("COUNTRY", "DAY", "LATITUDE", "LOCATION_NAME", "LONGITUDE", "MONTH", "YEAR" , "bce"))
+
 #' Function is run to clean data from NOAA and transforms to prescribed form
 #'
-#' This function used to clean data from NOAA. File is loaded to dataframe from 
+#' This function used to clean data from NOAA. File is loaded to dataframe from
 #' TSV file in gz file. first part of function is used to create bce variable,
-#' which is used to determine which function will be used to transform to data. 
-#' In case that there is a negative sign then parse_bce_ymd will be used to transform 
-#' date variable. YEAR, MONTH and DAY will be transformed to string character and padded 
+#' which is used to determine which function will be used to transform to data.
+#' In case that there is a negative sign then parse_bce_ymd will be used to transform
+#' date variable. YEAR, MONTH and DAY will be transformed to string character and padded
 #' to have format YYYY MM DD. Creation of date is dependent on bce variable. If bce
-#' variable is TRUE then parse_bce_ymd function will be used. 
+#' variable is TRUE then parse_bce_ymd function will be used.
 #' Final part of function is used to transform LATITUDE and LONGITUDE to numerical
 #' format.
-#' 
+#'
 #'
 #' @param x A numeric vector of values
 #' @export
@@ -19,7 +21,7 @@
 #' dataframe <- read_delim(filename)
 #' eq_clean_data(dataframe)
 #' }
-#' 
+#'
 eq_clean_data <- function(dataframe) {
   dataframe %>%
     mutate(bce = grepl("-",as.character(YEAR)),
@@ -27,9 +29,9 @@ eq_clean_data <- function(dataframe) {
            MONTH = stringr::str_pad(as.character(tidyr::replace_na(MONTH,1)),2, side = "left", pad = "0"),
            DAY = stringr::str_pad(as.character(tidyr::replace_na(DAY,1)),2, side = "left", pad = "0"),
            date = paste0(YEAR,"-", MONTH, "-", DAY),
-           date = as_date(ifelse(bce, parse_bce_ymd(date), as.Date(date))),
+           date = lubridate::as_date(ifelse(bce, parse_bce_ymd(date), as.Date(date))),
            LATITUDE = as.numeric(LATITUDE),
            LONGITUDE = as.numeric(LONGITUDE)
-           
+
     )
 }
--- R/eq_location_clean.R
+++ R/eq_location_clean.R
@@ -1,10 +1,12 @@
+globalVariables(c("COUNTRY", "DAY", "LATITUDE", "LOCATION_NAME", "LONGITUDE", "MONTH", "YEAR" , "bce"))
+
 #' Cleaning variable LOCATION_NAME from inclusion of COUNTRY
 #'
 #' Function will be used to transform column LOCATION_NAME to clean form.
-#' First part of process is removing COUNTRY from LOCATION_NAME with function 
+#' First part of process is removing COUNTRY from LOCATION_NAME with function
 #' str_remove_all.
 #' Secund part is transforming LOCATION_NAME to title caps.
-#' Final part of function str_remove_all is used to replace : and ; 
+#' Final part of function str_remove_all is used to replace : and ;
 #' in LOCATION_NAME to clean string.
 #'
 #' @param dataframe Dataframe to be transformed
@@ -16,10 +18,10 @@
 #' dataframe <- eq_clean_data(dataframe)
 #' eq_clean_data(dataframe)
 #' }
-#' 
+#'
 eq_location_clean <- function(dataframe) {
   dataframe %>%
-    mutate(LOCATION_NAME = stringr::str_replace_all(stringr::str_replace_all(stringr::str_to_title(stringr::str_replace_all(LOCATION_NAME, COUNTRY, "")),":"," "), "; ", ""),
+    dplyr::mutate(LOCATION_NAME = stringr::str_replace_all(stringr::str_replace_all(stringr::str_to_title(stringr::str_replace_all(LOCATION_NAME, COUNTRY, "")),":"," "), "; ", ""),
            LOCATION_NAME = ifelse(LOCATION_NAME == "", paste("Only country is present:",stringr::str_to_title(COUNTRY)), LOCATION_NAME)
     )
 }
